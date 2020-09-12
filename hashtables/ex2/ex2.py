2b0c943f9f28f291208098be0517143ff9b5270c
Denvinn Magsino <dee@denvinns-mbp.home>
2020-09-12 11:32:07 -0700

    working on ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,22 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    my_dict = {}
+    for i in range(length - 1):
+        my_dict[tickets[i].source] = tickets[i].destination
+
+    route = [my_dict['NONE']]
+
+
+    print(route)
+    print(my_dict)
+    for i in range(length):
+        print(i)
+        if route[i] in my_dict:
+            route[i+1] = my_dict[route[i]]
+
+    # print(route)
 
     return route
+
+
