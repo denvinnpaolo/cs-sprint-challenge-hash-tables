8b10872488550cdf15513ce121755e6b349c6935
Denvinn Magsino <dee@Denvinns-MacBook-Pro.local>
2020-09-13 18:45:06 -0700

    finished ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,8 +3,16 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    d = {}
+    for n in a:
+        num = abs(n)
+        if num not in d:
+            d[num] = 1
+        else: 
+            d[num] += 1
 
-    return result
+    res = [key for (key, value) in d.items() if value > 1 ]
+    return res
 
 
 if __name__ == "__main__":
